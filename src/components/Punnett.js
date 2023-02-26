2aa91f4bccf5c04123b4597f88c4a7e444654cf7
Isaac Voehringer <isaacvoehringer@Isaacs-MacBook-Air.local>
2023-02-26 08:16:42 -0600

    adding dropdowns

--- src/components/Punnett.js
+++ src/components/Punnett.js
@@ -4,7 +4,7 @@ import PunnettSquare from 'punnett-square';
 
 const FrecklesTraits = ['F', 'f'];
 const Freckles = new PunnettSquare(FrecklesTraits);
-const MOM = ['F', 'f']
+const MOM = ['${ mallele1 }', '${mallele2}']
 const DAD = ['f', 'f']
 
 const cross = Freckles.cross(MOM, DAD);
@@ -13,12 +13,33 @@ export default function Punnett(){
     return(
         <>
     <h1>Punnett Square</h1>
-    <table>
-        <input></input>
-        <input></input>
-    </table>
+   
     <div>
         Your results are: {cross}
+    </div>
+    <div>
+        <p>Dad's Geneotype:
+            <label for ='dalleles'>Allele's</label>
+                <select name="dallele1" id="dallele1">
+                    <option value="F" selected>F</option>
+                    <option value="f"></option>
+                </select>
+                <select name="dallele2" id="dallele2">
+                    <option value="F" selected>F</option>
+                    <option value="f"></option>
+                </select>
+        </p>  
+        <p>Mom's Geneotype:
+            <label for ='malleles'>Allele's</label>
+                <select name="mallele1" id="mallele1">
+                    <option value="F" selected>F</option>
+                    <option value="f"></option>
+                </select>
+                <select name="mallele2" id="mallele2">
+                    <option value="F" selected>F</option>
+                    <option value="f"></option>
+                </select>
+        </p> 
     </div>
     </>
     )
