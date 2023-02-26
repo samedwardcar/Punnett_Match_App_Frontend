7251e764e9175b8e3fa253b9c08ac4e6ee5e20f5
Isaac Voehringer <isaacvoehringer@Isaacs-MacBook-Air.local>
2023-02-26 12:49:26 -0600

    punnett functionality and render

--- src/components/Punnett.js
+++ src/components/Punnett.js
@@ -1,69 +1,69 @@
-import React from "react";
-import PunnettSquare from 'punnett-square';
+// import React from "react";
+// import PunnettSquare from 'punnett-square';
 
 
-const FrecklesTraits = ['F', 'f'];
-const Freckles = new PunnettSquare(FrecklesTraits);
-const MOM = ['F', 'f']
-const DAD = ['f', 'f']
+// // const FrecklesTraits = ['F', 'f'];
+// // const Freckles = new PunnettSquare(FrecklesTraits);
+// // const MOM = ['F', 'f']
+// // const DAD = ['f', 'f']
 
-const cross = Freckles.cross(MOM, DAD);
+// // const cross = Freckles.cross(MOM, DAD);
 
-const DimplesTraits = ['Dimples', 'No dimples'];
-const Dimples = new PunnettSquare(DimplesTraits);
-const mommy = ['Dimples', 'No dimples']
-const daddy = ['No dimples', 'No dimples']
+// const DimplesTraits = ['Dimples', 'No dimples'];
+// const Dimples = new PunnettSquare(DimplesTraits);
+// const mommy = ['Dimples', 'No dimples']
+// const daddy = ['No dimples', 'No dimples']
 
-const DimplesCross = Dimples.cross(mommy, daddy)
-// var dallele1Value = document.getElementById('dallele1')
-var dalleles = [ document.getElementById("dallele1"), document.getElementById("dallele2") ]
-// console.log(dallele1Value.value())
-var malleles = [ document.getElementById("mallele1'"), document.getElementById("mallele2") ]
+// const DimplesCross = Dimples.cross(mommy, daddy)
+// // var dallele1Value = document.getElementById('dallele1')
+// var dalleles = [ document.getElementById("dallele1"), document.getElementById("dallele2") ]
+// // console.log(dallele1Value.value())
+// var malleles = [ document.getElementById("mallele1'"), document.getElementById("mallele2") ]
 
-const FrecklesDynamic = new PunnettSquare(FrecklesTraits);
-var mom = { malleles };
-var dad = { dalleles };
+// const FrecklesDynamic = new PunnettSquare(FrecklesTraits);
+// var mom = { malleles };
+// var dad = { dalleles };
 
-console.log({ dalleles })
+// console.log({ dalleles })
 
-const dynamicCross = FrecklesDynamic.cross(mom, dad);
+// const dynamicCross = FrecklesDynamic.cross(mom, dad);
 
-export default function Punnett(){
-    return(
-        <>
-    <h1>Punnett Square</h1>
+export default function Punnett(){}
+//     return(
+//         <>
+//     <h1>Punnett Square</h1>
    
-    <div>
-        Static results are: {cross}
-      Dimples results are: {DimplesCross}
-    </div>
-    <div>
-        <p>Dad's Geneotype:
-            <label htmlFor ='dalleles'>Allele's</label>
-                <select name="dallele1" id="dallele1">
-                    <option defaultValue="F">F</option>
-                    <option value="f">f</option>
-                </select>
-                <select name="dallele2" id="dallele2">
-                    <option value="F">F</option>
-                    <option defaultValue="f">f</option>
-                </select>
-        </p>  
-        <p>Mom's Geneotype:
-            <label htmlFor ='malleles'>Allele's</label>
-                <select name="mallele1" id="mallele1">
-                    <option defaultValue="F">F</option>
-                    <option value="f">f</option>
-                </select>
-                <select name="mallele2" id="mallele2">
-                    <option value="F">F</option>
-                    <option defaultValue="f">f</option>
-                </select>
-        </p> 
+//     <div>
+//         Static results are: {cross}
+//       Dimples results are: {DimplesCross}
+//     </div>
+//     <div>
+//         <p>Dad's Geneotype:
+//             <label htmlFor ='dalleles'>Allele's</label>
+//                 <select name="dallele1" id="dallele1">
+//                     <option defaultValue="F">F</option>
+//                     <option value="f">f</option>
+//                 </select>
+//                 <select name="dallele2" id="dallele2">
+//                     <option value="F">F</option>
+//                     <option defaultValue="f">f</option>
+//                 </select>
+//         </p>  
+//         <p>Mom's Geneotype:
+//             <label htmlFor ='malleles'>Allele's</label>
+//                 <select name="mallele1" id="mallele1">
+//                     <option defaultValue="F">F</option>
+//                     <option value="f">f</option>
+//                 </select>
+//                 <select name="mallele2" id="mallele2">
+//                     <option value="F">F</option>
+//                     <option defaultValue="f">f</option>
+//                 </select>
+//         </p> 
         
-        Dynamic results are: {dynamicCross}
-    </div>
-    </>
-    )
-}
+//         Dynamic results are: {dynamicCross}
+//     </div>
+//     </>
+//     )
+// }
 
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,25 +1,104 @@
-import React from "react"
-// import { useState } from "react";
 
+import React , {useState} from "react";
+import PunnettSqure from 'punnett-square';
 
 const Square = () => {
-    const [selectValue, setSelectValue] = React.useState("");
-    const onChange = (event) => {
-        const value = event.tatget.value;
-        setSelectValue(value);
+    const [paternalFreckleValue, setPaternalFreckleValue] = useState('Freckles');
+    const [maternalFreckleValue, setMaternalFreckleValue] = useState('Freckles');
+    const [paternalBaldness, setPaternalBaldness] = useState(null);
+    const [maternalBaldness, setMaternalBaldness] = useState(null);
+    const [paternalDimples, setPaternalDimples] = useState(null);
+    const [maternalDimples, setMaternalDimples] = useState(null);
+    const [square, setSquare] = useState([])
+    const onChange = (event, updateFunction) => {
+        console.log(event.target.value)
+        const value = event.target.value;
+        updateFunction(value);
     };
+
+    const handleCalulation = () => {
+        const FreckleTraits = ['Freckles', 'No Freckles'];
+        const freckleSquare = new PunnettSqure(FreckleTraits);
+        const MOM = [maternalFreckleValue, paternalFreckleValue];
+        const DAD = [paternalFreckleValue, maternalFreckleValue];
+        const cross = freckleSquare.cross(MOM, DAD)
+        console.log(maternalFreckleValue, paternalFreckleValue)
+        setSquare(cross)
+          console.log(  { cross } )
+        
+
+
+    }
+
+    const style = {
+        border: 'solid',
+
+    }
 
     return(
         <>
         <h1>Square Page punnett</h1>
-        <select onChange={onChange} className='form-select'>
-            <option>
-                Select Phenotype
-            </option>
-            <option value="Freckles">Freckles</option>
-            <option value="No Freckles">No Freckles</option>
-        </select>
-            {selectValue && <h2>{selectValue}</h2>}
+        <div>
+            <label>Father: Freckles or not?</label>
+            <select onChange={(e) => onChange(e, setPaternalFreckleValue)} className='form-select'>
+            
+                <option value="Freckles">Freckles</option>
+                <option value="No Freckles">No Freckles</option>
+            </select>
+            {paternalFreckleValue && <h2>{paternalFreckleValue}</h2>}
+            <label>Mother: Freckles or not?</label>
+            <select onChange={(e) => onChange(e, setMaternalFreckleValue)} className='form-select'>
+            
+                <option value="Freckles">Freckles</option>
+                <option value="No Freckles">No Freckles</option>
+            </select>
+            {maternalFreckleValue && <h2>{maternalFreckleValue}</h2>}
+        </div>
+        <div>
+            <label>Father Bald or not?</label>
+            <select onChange={(e) => onChange(e, setPaternalBaldness)} className='form-select'>
+                <option value="Bald">Bald</option>
+                <option value="Not Bald">Not Bald</option>
+            </select>
+            {paternalBaldness && <h2>{paternalBaldness}</h2>}
+
+            <label>Mother's father Bald or not?</label>
+            <select onChange={(e) => onChange(e, setMaternalBaldness)} className='form-select'>
+                <option value="Bald">Bald</option>
+                <option value="Not Bald">Not Bald</option>
+            </select>
+            {maternalBaldness && <h2>{maternalBaldness}</h2>}
+        </div>
+        <div>
+            <label>Father Dimples?</label>
+            <select onChange={(e) => onChange(e, setPaternalDimples)} className='form-select'>
+                <option value="Dimples">Dimples</option>
+                <option value="No Dimples">No Dimples</option>
+            </select>
+            <h2>{paternalDimples}</h2>
+            <label>Mother Dimples?</label>
+            <select onChange={(e) => onChange(e, setMaternalDimples)} className='form-select'>
+                <option value="Dimples">Dimples</option>
+                <option value="No Dimples">Not Bald</option>
+            </select>
+            <h2>{maternalDimples}</h2>
+        </div>
+        <div>
+            <button onClick={handleCalulation}>Calculate</button>
+            
+        </div>
+        <div>
+            {square.length > 0 &&   
+                square.map((elem, idx) => {
+                    return (
+                        <div style={style} key={`box=${idx}`}>
+                            <span>{elem[0]}</span>
+                            <span>{elem[1]}</span>
+                        </div>
+                    )
+                })
+            }
+        </div>
         </>
     )
 }
