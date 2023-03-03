04c5526af1604a02d9c86a06f94caaa900ed737e
sixtoherrera <sixto.herrera@continental.com>
2023-03-02 19:30:28 -0600

    modified:   src/database/tables-create.sql~ by adding a table named Users

--- src/database/tables-create.sql
+++ src/database/tables-create.sql
@@ -9,6 +9,23 @@ CREATE TABLE public.variables
     m_dimples boolean NOT NULL,
     PRIMARY KEY (user_id)
 );
+CREATE TABLE public."Users"
+(
+    user_info serial NOT NULL,
+    email character varying(30) NOT NULL,
+    password character varying(30) NOT NULL,
+    first_name character(30) NOT NULL,
+    last_name character(30) NOT NULL,
+    PRIMARY KEY (user_info),
+    CONSTRAINT id_fk FOREIGN KEY (user_info)
+        REFERENCES public.variables (user_id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+        NOT VALID
+);
 
+ALTER TABLE IF EXISTS public."Users"
+    OWNER to postgres;
+    
 ALTER TABLE IF EXISTS public.variables
     OWNER to postgres;
