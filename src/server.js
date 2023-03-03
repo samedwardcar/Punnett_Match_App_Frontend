272960f13e401e9777114a4fcac1844ac399a00c
sixtoherrera <sixto.herrera@continental.com>
2023-03-02 19:14:58 -0600

    added server.js modified package.lock.json to add sequelize, added .env file and modified package.json

---
+++ src/server.js
@@ -0,0 +1,31 @@
+// DEPENDENCIES
+const express = require('express')
+const app = express()
+const { Sequelize } = require('sequelize')
+// SEQUELIZE CONNECTION
+const sequelize = new Sequelize(process.env.PG_URI)
+
+try {
+    sequelize.authenticate() 
+    console.log(`Connected with Sequelize at ${process.env.PG_URI}`) 
+} catch(err) {
+    console.log(`Unable to connect to PG: ${err}`) 
+}
+
+
+// CONFIGURATION / MIDDLEWARE
+require('dotenv').config()
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+// ROOT
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome to the Tour API'
+    })
+})
+
+// LISTEN
+app.listen(process.env.PORT, () => {
+    console.log(`🎸 Rockin' on port: ${process.env.PORT}`)
+})
